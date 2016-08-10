f403cb72fd6161f22e4a24a8df42dd4a60cb66fa
libra <chenqiuxia@xywy.com>
2016-08-10 10:45:52 +0800

    添加俱乐部冠名、俱乐部资料、添加运动页的表单验证

--- js/top.js
+++ js/top.js
@@ -65,4 +65,107 @@
             $(this).removeClass('active');
         }
     });
+
+    //俱乐部冠名验证
+    $('.club-t-form').on('submit', function(){
+        var titleVal = $('.club-t-name-title').val(),
+            nameVal = $('.club-t-name-pro').val();
+        if(titleVal == ''){
+            $('.club-t-name-title').parent().siblings('.club-error').html('冠名商名称不能为空！').show();
+            return false;
+        }else{
+            $('.club-t-name-title').parent().siblings('.club-error').hide();
+        }
+        if(nameVal == ''){
+            $('.club-t-name-pro').parent().siblings('.club-error').html('代表队名称不能为空！').show();
+            return false;
+        }else{
+            $('.club-t-name-pro').parent().siblings('.club-error').hide();
+        }
+    });
+
+    //俱乐部资料验证
+    $('.club-d-form').on('submit', function(){
+        var titleVal = $('.club-d-name').val(),
+            perVal = $('.club-d-per').val(),
+            relateVal = $('.club-d-relate').val(),
+            phoneVal = $('.club-d-phone').val(),
+            phoneReg = /^((0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/;
+
+        if(titleVal == ''){
+            $('.club-d-name').parent().siblings('.club-error').html('名称不能为空！').show();
+            return false;
+        }else{
+            $('.club-d-name').parent().siblings('.club-error').hide();
+        }
+        if(perVal == ''){
+            $('.club-d-per').parent().siblings('.club-error').html('法人不能为空！').show();
+            return false;
+        }else{
+            $('.club-d-per').parent().siblings('.club-error').hide();
+        }
+        if(relateVal == ''){
+            $('.club-d-relate').parent().siblings('.club-error').html('联系人不能为空！').show();
+            return false;
+        }else{
+            $('.club-d-relate').parent().siblings('.club-error').hide();
+        }
+        if(phoneVal == '' || !phoneReg.test(phoneVal)){
+            $('.club-d-phone').parent().siblings('.club-error').html('请输入正确的电话！').show();
+            return false;
+        }else{
+            $('.club-d-phone').parent().siblings('.club-error').hide();
+        }
+    });
+
+    //添加运动员验证
+    $('.club-a-form').on('submit', function(){
+        var nameVal = $('.club-a-name').val(),
+            authVal = $('.club-a-auth').val(),
+            typeVal = $('.club-a-select').find('.club-a-selected em').html(),
+            mailVal = $('.club-a-mail').val(),
+            mailReg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
+
+        if(nameVal == ''){
+            $('.club-a-name').parent().siblings('.club-error').html('姓名不能为空！').show();
+            return false;
+        }else{
+            $('.club-a-name').parent().siblings('.club-error').hide();
+        }
+        if(authVal == ''){
+            $('.club-a-auth').parent().siblings('.club-error').html('证件号码不能为空！').show();
+            return false;
+        }else{
+            $('.club-a-auth').parent().siblings('.club-error').hide();
+        }
+        if(typeVal == '请选择'){
+            $('.club-a-select').parent().siblings('.club-error').html('请选择身份类型！').show();
+            return false;
+        }else{
+            $('.club-a-select').parent().siblings('.club-error').hide();
+        }
+        if(mailVal == '' || !mailReg.test(mailVal)){
+            $('.club-a-mail').parent().siblings('.club-error').html('请输入正确的邮箱！').show();
+            return false;
+        }else{
+            $('.club-a-mail').parent().siblings('.club-error').hide();
+        }
+    });
+
+    //性别、身份类型的隐藏域赋值
+    $('.club-a-sex .check').on('click', function(){
+        if($(this).siblings().html() == '男' ){
+            $('.club-hidden-sex').val('男');
+        }else{
+            $('.club-hidden-sex').val('女');
+        }
+    });
+
+    $('.club-a-select .club-a-select-item').find('li').on('click', function(){
+        if($(this).text() == '中国大陆'){
+            $('.club-hidden-type').val('中国大陆');
+        }else if($(this).text() == '港澳台地区'){
+            $('.club-hidden-type').val('港澳台地区');
+        }
+    });
 })();
