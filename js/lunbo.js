4f0ee4b5afe0205e15a6e83f920b2f4559fa8b84
libraAmeng <510306899@qq.com>
2016-08-13 19:23:05 +0800

    合并到master

---
+++ js/lunbo.js
@@ -0,0 +1,35 @@
+
+
+
+$(document).ready(function(){
+
+    $('.con-slide-pic').carousel({
+        interval: 3000
+    })
+    /*// 初始化轮播
+    $(".start-slide").click(function(){
+        $("#myCarousel").carousel('cycle');
+    });
+    // 停止轮播
+    $(".pause-slide").click(function(){
+        $("#myCarousel").carousel('pause');
+    });
+    // 循环轮播到上一个项目
+    $(".prev-slide").click(function(){
+        $("#myCarousel").carousel('prev');
+    });
+    // 循环轮播到下一个项目
+    $(".next-slide").click(function(){
+        $("#myCarousel").carousel('next');
+    });
+    // 循环轮播到某个特定的帧
+    $(".slide-one").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".slide-two").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".slide-three").click(function(){
+        $("#myCarousel").carousel(2);
+    });*/
+})
